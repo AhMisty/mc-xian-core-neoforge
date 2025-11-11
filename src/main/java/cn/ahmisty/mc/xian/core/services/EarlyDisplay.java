24c377466ad55274bf17b16163ccc1986e91a0fa
AhMisty <AhMisty@outlook.com>
2025-11-11 18:29:17 +0800

    feat: 🎸 添加调用本地动态库

--- src/main/java/cn/ahmisty/mc/xian/core/services/EarlyDisplay.java
+++ src/main/java/cn/ahmisty/mc/xian/core/services/EarlyDisplay.java
@@ -1,5 +1,6 @@
 package cn.ahmisty.mc.xian.core.services;
 
+import cn.ahmisty.mc.xian.utils.Library;
 import com.google.auto.service.AutoService;
 import net.neoforged.fml.loading.FMLConfig;
 import net.neoforged.neoforgespi.earlywindow.GraphicsBootstrapper;
@@ -8,6 +9,9 @@ import net.neoforged.neoforgespi.locating.IOrderedProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.Field;
 import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,6 +23,7 @@ public class EarlyDisplay implements GraphicsBootstrapper, ImmediateWindowProvid
     public static final String NAME = "xian";
     private static final Logger LOGGER = LoggerFactory.getLogger(NAME);
     private static ImmediateWindowProvider PROVIDER;
+    private long WINDOW;
 
     @Override
     public String name () {
@@ -33,25 +38,41 @@ public class EarlyDisplay implements GraphicsBootstrapper, ImmediateWindowProvid
         return Integer.MAX_VALUE;
     }
 
+    public void initLink() {
+        Library library = new Library("xian.dll", Arena.ofConfined());
+        if (library.instance == null) return;
+        MethodHandle print = library.loadFunction("print", FunctionDescriptor.ofVoid());
+        try {
+            print.invokeExact();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void bootstrap(String[] arguments) {
         if (PROVIDER != null) return;
+        initLink();
         String ProviderName = FMLConfig.getConfigValue(FMLConfig.ConfigValue.EARLY_WINDOW_PROVIDER);
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
         ServiceLoader<ImmediateWindowProvider> serviceLoader = ServiceLoader.load(ImmediateWindowProvider.class, contextClassLoader);
-        PROVIDER = serviceLoader.stream()
-                .map(ServiceLoader.Provider::get)
-                .filter(p -> !Objects.equals(p.name(), NAME))
-                .filter(p -> Objects.equals(p.name(), ProviderName))
-                .sorted(Comparator.comparingInt(p -> (p instanceof IOrderedProvider op) ? op.getPriority() : 0).reversed())
-                .findFirst()
-                .orElse(null);
-        if (PROVIDER != null) {
-            LOGGER.info("Successfully found provider: {}", PROVIDER.name());
-        } else {
-            String message = "Could not find any other window provider.";
-            LOGGER.error(message);
-            throw new IllegalStateException(message);
+        try {
+            PROVIDER = serviceLoader.stream()
+                    .map(ServiceLoader.Provider::get)
+                    .filter(p -> !Objects.equals(p.name(), NAME))
+                    .filter(p -> Objects.equals(p.name(), ProviderName))
+                    .sorted(Comparator.comparingInt(p -> (p instanceof IOrderedProvider op) ? op.getPriority() : 0).reversed())
+                    .findFirst()
+                    .orElse(null);
+            if (PROVIDER != null) {
+                LOGGER.info("Successfully found provider: {}", PROVIDER.name());
+            } else {
+                String message = "Could not find provider: " + ProviderName;
+                LOGGER.error(message);
+                throw new RuntimeException(message);
+            }
+        } catch (Throwable _) {
+            LOGGER.info("Skip to load provider because launch target may be server.");
         }
     }
 
@@ -62,8 +83,23 @@ public class EarlyDisplay implements GraphicsBootstrapper, ImmediateWindowProvid
 
     @Override
     public Runnable initialize(String[] args) {
-        LOGGER.info("Use provider: {}", PROVIDER.name());
-        return PROVIDER.initialize(args);
+       LOGGER.info("Use provider: {}", PROVIDER.name());
+       Runnable runnable = PROVIDER.initialize(args);
+       try {
+           Field windowField = PROVIDER.getClass().getDeclaredField("window");
+           windowField.setAccessible(true);
+           WINDOW = (long) windowField.get(PROVIDER);
+           if (WINDOW == 0L) {
+               String message = "Parsed invalid window handle from provider: " + PROVIDER.name();
+               LOGGER.error(message);
+               throw new IllegalStateException(message);
+           }
+       } catch (NoSuchFieldException | IllegalAccessException e) {
+           LOGGER.error("Could not parse window handle from provider: {}", PROVIDER.name());
+           throw new RuntimeException(e);
+       }
+       LOGGER.info("Successfully parsed window handle {} from provider: {}", WINDOW, PROVIDER.name());
+       return runnable;
     }
 
     @Override
---
+++ src/main/java/cn/ahmisty/mc/xian/utils/Library.java
@@ -0,0 +1,77 @@
+package cn.ahmisty.mc.xian.utils;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.loading.FMLPaths;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+public class Library {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static Path BASE = FMLPaths.GAMEDIR.get().resolve("xian").resolve("libraries");
+    public final Path name;
+    public final Path dir;
+    public final Path path;
+    public final Arena arena;
+    public static final Linker LINKER = Linker.nativeLinker();
+    public final SymbolLookup instance;
+
+    public Library (String name, Arena arena) {
+        boolean initialized;
+        this.arena = arena;
+        this.name = Path.of(name).getFileName();
+        this.path = BASE.resolve(name);
+        this.dir = this.path.getParent();
+        if (!Files.exists(this.path)) {
+            Path pathInJar = Path.of("/libraries").resolve(name);
+            LOGGER.info("Could not find library {} at {}, try to find in jar path {}", this.name, this.dir, pathInJar);
+            try (InputStream stream = Library.class.getResourceAsStream(pathInJar.toString())) {
+                if (stream == null) {
+                    LOGGER.error("Could not find library {} in jar path {}", this.name, pathInJar);
+                    initialized = false;
+                } else {
+                    LOGGER.info("Successfully found library {} in jar path {}, try to copy it to {}", this.name, pathInJar, this.path);
+                    Files.createDirectories(this.dir);
+                    Files.copy(stream, this.path, StandardCopyOption.REPLACE_EXISTING);
+                    LOGGER.info("Successfully copied library {} to {}", this.name, this.path);
+                    initialized = true;
+                }
+            } catch (IOException error) {
+                LOGGER.error("Could not find library {} with error {}", this.name, error);
+                initialized = false;
+            }
+        } else {
+            LOGGER.info("Successfully found library {} at {}", this.name, this.dir);
+            initialized = true;
+        }
+        if (initialized) {
+            this.instance = SymbolLookup.libraryLookup(this.path, arena);
+            LOGGER.info("Successfully loaded library {} at {}", this.name, this.path);
+        } else {
+            this.instance = null;
+        }
+    }
+
+    public MethodHandle loadFunction (String name, FunctionDescriptor descriptor, Linker.Option option) {
+        Optional<MemorySegment> symbol = this.instance.find(name);
+        if (symbol.isEmpty()) {
+            return null;
+        }
+        return LINKER.downcallHandle(symbol.get(), descriptor, option);
+    }
+
+    public MethodHandle loadFunction (String name, FunctionDescriptor descriptor) {
+        Optional<MemorySegment> symbol = this.instance.find(name);
+        if (symbol.isEmpty()) {
+            return null;
+        }
+        return LINKER.downcallHandle(symbol.get(), descriptor);
+    }
+}
